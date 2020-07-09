94154ee54407243e90ffc5e50834fe1fcace1501
Baha <bahablgn@gmail.com>
2020-07-09 00:36:02 -0400

    second

--- setup.py
+++ setup.py
@@ -1,14 +1,15 @@
 from setuptools import find_packages, setup
 
 
-setup(name="keras_segmentation",
+setup(name="kis",
       version="0.3.0",
-      description="Image Segmentation toolkit for keras",
-      author="Divam Gupta",
-      author_email='divamgupta@gmail.com',
+      description="Image Segmentation toolkit for keras edited for work",
+      author="Baha",
+      author_email='bahablgn@gmail.com',
       platforms=["any"],  # or more specific, e.g. "win32", "cygwin", "osx"
       license="GPLv3",
-      url="https://github.com/divamgupta/image-segmentation-keras",
+      url="https://github.com/bahablgn/kis",
+      download_url = 'https://github.com/bahablgn/kis/archive/v_01.tar.gz',
       packages=find_packages(exclude=["test"]),
       entry_points={
             'console_scripts': [
