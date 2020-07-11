087a41c26f542c8c2c90817c0f3018f83a12cbc6
Baha <bahablgn@gmail.com>
2020-07-11 14:08:36 -0400

    version edit

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from setuptools import find_packages, setup
 
 
 setup(name="kis",
-      version="0.6.0",
+      version="0.7.0",
       description="Image Segmentation toolkit for keras edited for work",
       author="Baha",
       author_email='bahablgn@gmail.com',
       platforms=["any"],  # or more specific, e.g. "win32", "cygwin", "osx"
       license="GPLv3",
       url="https://github.com/bahablgn/kis",
-      download_url = 'https://github.com/bahablgn/kis/archive/v_06.tar.gz',
+      download_url = 'https://github.com/bahablgn/kis/archive/v_07.tar.gz',
       packages=find_packages(exclude=["test"]),
       entry_points={
             'console_scripts': [
